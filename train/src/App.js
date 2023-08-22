4a9212529ea63edf60e609976b8244bb1390d80c
serz4862 <sauravkumar4862@gmail.com>
2023-08-22 20:16:31 +0530

    train schedule

--- train/src/App.js
+++ train/src/App.js
@@ -1,27 +1,34 @@
 import logo from './logo.svg';
-import {scheduleComponent} from '@syncfusion/ej2-react-schedule';
+import {Inject, ScheduleComponent,Day, Week, WorkWeek, Month ,Agenda} from '@syncfusion/ej2-react-schedule';
 
 function App() {
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends react.Component{
+   render(){
+    return <ScheduleComponent>
+      <Inject services = {[Day, Week, WorkWeek, Month, Agenda]}/>
+    </ScheduleComponent>
+  }
+}
   );
 }
-
 export default App;
