c4729fc0b72b6be65bcaa5fb09c46115547f1994
serz4862 <sauravkumar4862@gmail.com>
2023-08-22 19:15:23 +0530

    sorting of array using API payload

--- number/index.js
+++ number/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON in request bodies
+
 app.use(express.json());
 
-// Route to sort numbers
+
 app.post('/sort', (req, res) => {
     try {
         const numbers = req.body.numbers;
 
         if (!numbers || !Array.isArray(numbers)) {
-            return res.status(400).json({ error: 'Invalid input. Expected an array of numbers.' });
+            return res.status(400).json({ error: 'Invalid ' });
         }
 
         const sortedNumbers = numbers.slice().sort((a, b) => a - b);
